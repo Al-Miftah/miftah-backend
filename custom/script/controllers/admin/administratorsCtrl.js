5b5e73ddd226e53e00df63897871b1d3591d7e6e
Ibrahim Samad <ibrahim@walulel.com>
2019-01-21 20:50:39 +0000

    Remove legacy code

--- custom/script/controllers/admin/administratorsCtrl.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Aditech on 5/30/2016.
- */
-(function () {
-    'use strict';
-    angular.module("myApp").controller('administratorsCtrl', ['$scope','auth', function ($scope,auth) {
-
-       }])
-})();
