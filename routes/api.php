8ed0e51d4b7fa2ac5454fffb90b47f6dee775647
Ibrahim Samad <naatogma@gmail.com>
2020-07-10 14:34:47 +0000

    Refactor

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function(){
-    return redirect('/api/docs');
-});
+Route::redirect('/', '/api/docs');
 
 //User authentication
 Route::post('user/auth/register', 'Auth\API\RegisterController@register')->name('user.auth.register');
